57de4d47c41b0c2708f91e86217398433e1a723b
code-abhash <abhashraj751@gmail.com>
2024-06-20 23:55:28 +0530

    logged user can use

--- frontend/utils/PrivateRoute.jsx
+++ frontend/utils/PrivateRoute.jsx
@@ -1,11 +1,23 @@
-import {Route, Redirect} from "react-router-dom"
-import {useContext} from "react"
-import AuthContext from "../Auth_contxt/Authcontext"
+// import {Route, Redirect} from "react-router-dom"
+// import {useContext} from "react"
+// import AuthContext from "../Auth_contxt/Authcontext"
 
 
-const PrivateRoute = ({children, ...rest}) => {
-    let {user} = useContext(AuthContext)
-    return <Route {...rest}>{!user ? <Redirect to="/login" /> : children}</Route>
-}
+// const PrivateRoute = ({children, ...rest}) => {
+//     let {user} = useContext(AuthContext)
+//     return <Route {...rest}>{!user ? <Redirect to="/login" /> : children}</Route>
+// }
 
-export default PrivateRoute
+// export default PrivateRoute
+
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import AuthContext from '../Auth_contxt/Authcontext';
+
+const PrivateRoute = ({ children }) => {
+    const { user } = useContext(AuthContext);
+
+    return user ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
